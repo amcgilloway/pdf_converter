dcdf49f8fb0ba0285b1ece47beb962bec5ccdc4e
Alastair McGilloway <alastairmcgilloway@hotmail.com>
2019-06-05 10:51:43 +0100

    Updates to be zip app

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const markdownpdf = require("markdown-pdf")
 const fs = require("fs")
 const walk = require('walk');
 const mv = require('mv');
+const zipFolder = require('zip-a-folder');
 
 // Change path to full path to local repo folder (e.g. /Users/username/course_java)
-const input_path = '/Users/allymcgilloway/courses/course_intro_to_programming';
+const input_path = '/Users/allymcgilloway/projects/vue';
 
 // Change to name of module as appears on Github
-const module_name = 'course_intro_to_programming';
+const module_name = 'vue';
 const options = {
   cssPath: "./css/github.css",
   remarkable: {
@@ -21,23 +22,19 @@ const options = {
 
 const walker  = walk.walk(input_path, { followLinks: false });
 
-walker.on('file', (root, file, next) => {
+walker.on('directory', (root, folder, next) => {
   let pathParts = root.split('/');
   let basePath = pathParts.slice(pathParts.indexOf(module_name)).join("/");
-  if (file.name.slice(-3) === '.md'){
-    let pdfDoc = file.name.replace(".md", ".pdf");
-    options.imagePath = root;
-    markdownpdf(options).from(root + `/${file.name}`).to(`./out/${basePath}/${pdfDoc}`, function () {
-      console.log("Created", pdfDoc);
-    });
-  } else {
-
-    fs.copyFile(root + `/${file.name}`, `./out/${basePath}/${file.name}`, (err) => {
-      if (err){
-        console.log("Error", err);
+  if (folder.name.includes('start') || folder.name.includes('end') || folder.name.includes('solution')){
+    zipFolder.zipFolder(root + '/' + folder.name, root + '/' + folder.name + '.zip', function(err) {
+            if(err) {
+                console.log('Something went wrong!', err);
+            }
+        });
+      } else {
+        console.log("NOT DONE");
       }
-    })
-  }
+
   next();
 });
 
